40aaf5cb66c3074774115a042643821ea9fe81e1
Christos Efstathiades <46204113+ChristosEfstathiades@users.noreply.github.com>
2026-04-09 13:49:21 +0100

    Update preload.ts

--- src/preload.ts
+++ src/preload.ts
@@ -5,4 +5,6 @@ contextBridge.exposeInMainWorld('electron', {
   getHistory: () => ipcRenderer.invoke('get-history'),
   deleteHistory: (id: number) => ipcRenderer.invoke('delete-history', id),
   getClipboard: () => ipcRenderer.invoke('get-clipboard'),
+  getQueue: () => ipcRenderer.invoke('get-queue'),
+  getCurrentDownloading: () => ipcRenderer.invoke('get-current-downloading'),
 });
